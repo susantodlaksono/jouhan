52db96d1033d28c91fa41e6ed089812bf4df0a95
Susanto Dwilaksono <susantodlaksono@gmail.com>
2020-01-10 21:53:34 +0700

    crawler

--- application/views/report/_client_twitter.php
+++ application/views/report/_client_twitter.php
@@ -15,19 +15,19 @@ $pe->setActiveSheetIndex(0);
 $cell = $pe->getActiveSheet();
 
 ##Style
-// foreach ($this->mapping_report->excelColumnRange('A', 'I') as $column) {
-// 	$cell->getColumnDimension($column)->setAutoSize(true);
-// }
+foreach ($this->mapping_report->excelColumnRange('A', 'I') as $column) {
+    $cell->getColumnDimension($column)->setAutoSize(true);
+}
 $cell->getStyle('A1:I1')->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
 $cell->getStyle('A1:I1')->getAlignment()->setVertical(PHPExcel_Style_Alignment::VERTICAL_CENTER);
 $cell->getStyle('A1:I1')->getFont()->setBold(TRUE);
 $cell->getStyle('A1:I1')->applyFromArray(
-	array(
-    	'fill' => array(
+    array(
+        'fill' => array(
         'type' => PHPExcel_Style_Fill::FILL_SOLID,
         'color' => array('rgb' => 'f2f2f2')
-    	)
-	)
+        )
+    )
 );
 
 
@@ -42,20 +42,18 @@ $cell->setCellValue('H1', 'Proxy');
 $cell->setCellValue('I1', 'Status');
 
 $i = 3;
-// if($report){
-//     foreach ($report->result_array() as $v) {
-//     	$cell->setCellValue('A'.$i, $v['first_name']);
-//     	$cell->setCellValue('B'.$i, date('Y-m-d', strtotime($v['created_twitter'])));
-//     	$cell->setCellValue('C'.$i, $v['phone_number']);
-//     	$cell->setCellValue('D'.$i, $v['email']);
-//     	$cell->setCellValue('E'.$i, $v['screen_name']);
-//     	$cell->setCellValue('F'.$i, $v['password']);
-//     	$cell->setCellValue('G'.$i, $v['client_name']);
-//     	$cell->setCellValue('H'.$i, $v['proxy_name']);
-//     	$cell->setCellValue('I'.$i, $v['status_name']);
-//     	$i++;
-//     }
-// }
+foreach ($report->result_array() as $v) {
+    $cell->setCellValue('A'.$i, $v['first_name']);
+    $cell->setCellValue('B'.$i, date('Y-m-d', strtotime($v['created_twitter'])));
+    $cell->setCellValue('C'.$i, $v['phone_number']);
+    $cell->setCellValue('D'.$i, $v['email']);
+    $cell->setCellValue('E'.$i, $v['screen_name']);
+    $cell->setCellValue('F'.$i, $v['password']);
+    $cell->setCellValue('G'.$i, $v['client_name']);
+    $cell->setCellValue('H'.$i, $v['proxy_name']);
+    $cell->setCellValue('I'.$i, $v['status_name']);
+    $i++;
+}
 // $cell->setAutoFilter('A2:I2');
 
 $cell->setTitle('Raw Data');
@@ -66,20 +64,20 @@ $writer = PHPExcel_IOFactory::createWriter($pe,'Excel2007');
 $mode = isset($mode) ? $mode : 'download';
 
 switch ($mode) {
- 	case 'download':
-     	header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-     	header('Content-Disposition: attachment;filename="'.$fname.'"');
-     	header('Cache-Control: max-age=0');
-     	header('Cache-Control: max-age=1');
-     	header('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
-     	header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT'); // always modified
-     	header('Cache-Control: cache, must-revalidate'); // HTTP/1.1
-     	header('Pragma: public'); // HTTP/1.0
-     	$writer->save('php://output');
-     	exit;
-     	break;
- 	case 'save':
-     	$writer->save($filepath);
-     	echo $filepath;
-     	break;
+    case 'download':
+        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        header('Content-Disposition: attachment;filename="'.$fname.'"');
+        header('Cache-Control: max-age=0');
+        header('Cache-Control: max-age=1');
+        header('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+        header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT'); // always modified
+        header('Cache-Control: cache, must-revalidate'); // HTTP/1.1
+        header('Pragma: public'); // HTTP/1.0
+        $writer->save('php://output');
+        exit;
+        break;
+    case 'save':
+        $writer->save($filepath);
+        echo $filepath;
+        break;
 }
